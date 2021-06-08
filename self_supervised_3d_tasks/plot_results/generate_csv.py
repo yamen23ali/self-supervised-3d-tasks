f6dd3bb656098038d5272f87a889534dc225ede0
Yamen <yamen.ali@sap.com>
2021-06-08 20:00:57 +0200

    handle brats

--- self_supervised_3d_tasks/plot_results/generate_csv.py
+++ self_supervised_3d_tasks/plot_results/generate_csv.py
@@ -17,7 +17,12 @@ with open(filename) as f:
     lines = f.readlines()
 
 splits = ['5%', '10%', '25%', '50%', '100%']
-headers = ['Train Split', 'avg_dice', 'avg_dice_0', 'avg_dice_1', 'avg_dice_2']
+#headers = ['Train Split', 'avg_dice', 'avg_dice_0', 'avg_dice_1', 'avg_dice_2']
+#entries = ['dice', 'jaccard', 'dice_0', 'dice_1', 'dice_2']
+
+headers = ['Train Split', 'avg_dice', 'avg_brats_wt', 'avg_brats_tc', 'avg_brats_et']
+entries = ['dice', 'jaccard', 'brats_wt', 'brats_tc', 'brats_et']
+
 repetetion_per_split = 3
 
 write_result(base_path,headers)
@@ -26,34 +31,33 @@ line_index = 0
 for split in splits:
 	row = [split]
 	repetions_scores = {
-		'dice': [], 
-		'jaccard': [],
-		'dice_0': [],
-		'dice_1': [],
-		'dice_2': []
+		entries[0]: [],
+		entries[1]: [],
+		entries[2]: [],
+		entries[3]: [],
+		entries[4]: []
 	}
 
 	while True:
 		line = lines[line_index]
-		
+
 		if line.startswith('[('):
 			parts = line.split(',')
-			repetions_scores['dice'].append(float(parts[1][:-3]))
-			repetions_scores['jaccard'].append(float(parts[3][:-3]))
-			repetions_scores['dice_0'].append(float(parts[5][:-3]))
-			repetions_scores['dice_1'].append(float(parts[7][:-3]))
-			repetions_scores['dice_2'].append(float(parts[9][:-3]))
-		
+			repetions_scores[entries[0]].append(float(parts[1][:-3]))
+			repetions_scores[entries[1]].append(float(parts[3][:-3]))
+			repetions_scores[entries[2]].append(float(parts[5][:-3]))
+			repetions_scores[entries[3]].append(float(parts[7][:-3]))
+			repetions_scores[entries[4]].append(float(parts[9][:-3]))
+
 		line_index+=1
 
-		if len(repetions_scores['dice'])==3: break
-	
-	row.append(np.average(repetions_scores['dice']))
-	row.append(np.average(repetions_scores['dice_0']))
-	row.append(np.average(repetions_scores['dice_1']))
-	row.append(np.average(repetions_scores['dice_2']))
+		if len(repetions_scores[entries[0]])==3: break
+
+	row.append(np.average(repetions_scores[entries[0]]))
+	row.append(np.average(repetions_scores[entries[2]]))
+	row.append(np.average(repetions_scores[entries[3]]))
+	row.append(np.average(repetions_scores[entries[4]]))
 
 	write_result(base_path, row)
-	
 
-	
+
